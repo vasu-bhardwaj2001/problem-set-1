f824bd1ee7465fdd6ddf39d6add97d7e6bec353c
Ryan Wilson <rwilson@ucvts.org>
2018-09-14 14:02:47 -0400

    added skeleton code for problem set 1

---
+++ src/ProblemSet1.java
@@ -0,0 +1,27 @@
+/**
+ * INSTRUCTIONS.
+ * 
+ * Problem Set 1 will test your skills of printing to the console. You'll
+ * be asked to print various text, shapes, and figures, all of which is
+ * explained (and demonstrated) in detail in the README file.
+ */
+
+public class ProblemSet1 {	
+	
+	/**
+	 * Every Java program must have a <code>main</code> method, which is
+	 * where program execution begins.
+	 */
+	
+	public static void main(String[] args) {
+		System.out.println("/// EXERCISE 1 /////////////////////////////////////////////////\n");
+		
+		System.out.println("/// EXERCISE 2 /////////////////////////////////////////////////\n");
+		
+		System.out.println("/// EXERCISE 3 /////////////////////////////////////////////////\n");
+		
+		System.out.println("/// EXERCISE 4 /////////////////////////////////////////////////\n");
+		
+		System.out.println("/// EXERCISE 5 /////////////////////////////////////////////////\n");
+	}
+}
